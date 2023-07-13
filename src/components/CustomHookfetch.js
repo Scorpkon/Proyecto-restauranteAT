d1795cdc248af00822db1e94556de004b54f7d14
Scorpkon <aguilajsb@gmail.com>
2023-07-13 11:57:59 -0500

    Actualizacion del Hook para personalizar fetch

--- src/components/CustomHookfetch.js
+++ src/components/CustomHookfetch.js
@@ -1,9 +1,12 @@
+ import { useEffect, useState } from "react";
 
-
-export function CustomHookFetch(url){
-    fetch(url)
-    .then()
-    .catch()
-return "hola";
-
-}
+export const useFetch =(url)=>{
+    const [data, setData]=useState(null);
+    //fetch: Promesa donde yo recibo el dato y dentor le indico que es lo que quiero que haga 
+    useEffect(()=>{
+        fetch(url)
+        .then((res)=> res.json())
+        .then((data)=> setData(data))
+    }, [url]);
+    return [data];
+};
